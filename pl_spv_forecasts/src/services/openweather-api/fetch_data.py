f303a3bc09968940dd5ae442f4a59820ea65bf5a
klaudiabanaszczyk <klaudiabanaszczyk.poczta@gmail.com>
2024-08-16 19:54:18 +0200

    add new method of forecast data processing

--- pl_spv_forecasts/src/services/openweather-api/fetch_data.py
+++ pl_spv_forecasts/src/services/openweather-api/fetch_data.py
@@ -1,3 +1,5 @@
+from pandas import DataFrame, DatetimeIndex
+from datetime import datetime, date, timedelta
 from dataclasses import dataclass
 import requests
 
@@ -5,7 +7,7 @@ import requests
 @dataclass
 class OpenWeatherForecasts:
     """
-    A class to retrieve forecast weather data from the OpenWeather API.    
+    A class to retrieve forecast weather data from the OpenWeather API.
     """
 
     base_url = 'https://api.openweathermap.org/data/3.0/onecall?'
@@ -13,9 +15,20 @@ class OpenWeatherForecasts:
     def call_openweather_api(self, lat: float, lon: float, api_key: str) -> requests.Response:
         return requests.get(f'{self.base_url}lat={lat}&lon={lon}&exclude=minutely,daily&appid={api_key}')
 
+    def fetch_openweather_forecast_data(self, api_response: requests.Response) -> DataFrame:
+        """
 
-    def fetch_openweather_forecast_data(self, api_response):
-        pass
+        """
+        response_json = api_response.json()
+        df = DataFrame(response_json['hourly'])
+        df['lat'] = response_json['lat']
+        df['lon'] = response_json['lon']
+        df['timezone'] = response_json['timezone']
+        df['dt'] = df['dt'].apply(datetime.fromtimestamp)
+        df.index = DatetimeIndex(df['dt']).tz_localize('UTC')
+        df.index.name = 'datetime'
+        df['publication_date'] = datetime.now()
+        return df
 
 
 @dataclass
@@ -24,10 +37,26 @@ class OpenWeatherArchiveData:
     A class to retrieve historical weather data from the OpenWeather API.
     """
 
-    base_url = 'https://api.openweathermap.org/data/3.0/timemachine?'
+    base_url = 'https://history.openweathermap.org/data/2.5/history/city?'
 
-    def call_openweather_api(self, lat: float, lon: float, time: int, api_key: str) -> requests.Response:
-        return requests.get(f'{self.base_url}lat={lat}&lon={lon}&dt={time}&appid={api_key}')
+    def transform_datetime_to_timestampt(self, _date: datetime) -> int:
+        return int(datetime.timestamp(_date))
+
+    def call_openweather_api(self, lat: float, lon: float, start_time: int, end_time: int,
+                             api_key: str) -> requests.Response:
+        return requests.get(
+            f'{self.base_url}lat={lat}&lon={lon}&type=hour&start={start_time}&end={end_time}&appid={api_key}')
 
     def fetch_openweather_archive_data(self, api_response):
         pass
+
+
+def forecast_main():
+    api_key = ''
+    lat = 52.286
+    lon = 20.8
+
+    forecasts = OpenWeatherForecasts()
+    response = forecasts.call_openweather_api(lat, lon, api_key)
+    data = forecasts.fetch_openweather_forecast_data(response)
+    return data
