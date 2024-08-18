40bb2b46987086c70a4d2376c60ff0fda2272c0e
klaudiabanaszczyk <klaudiabanaszczyk.poczta@gmail.com>
2024-08-18 21:55:15 +0200

    change response as a global var

--- pl_spv_forecasts/tests/pse_api_test.py
+++ pl_spv_forecasts/tests/pse_api_test.py
@@ -2,21 +2,18 @@
 from pl_spv_forecasts.src.services.pse_data.api.fetch_data import PseApi
 from datetime import date
 
+response = PseApi().call_pse_api(date(2024, 6, 14))
+
 
 def test_status_code():
-    response = PseApi().call_pse_api(date(2024, 6, 14))
     assert response.status_code == 200
 
 
-# structure
 def test_structure():
-    response = PseApi().call_pse_api(date(2024, 6, 14))
     assert response.headers["Content-Type"] == 'application/json; charset=utf-8'
 
 
-# data integrity
 def test_data_integrity():
-    response = PseApi().call_pse_api(date(2024, 6, 14))
     data = response.json()
     assert isinstance(data, dict)
     assert "value" in data
